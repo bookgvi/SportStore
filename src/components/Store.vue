e61b677753b54b8af61cd7102cbbe16c34c46d1b
bookgvi <bookgvi@gmail.com>
2019-09-01 18:47:55 +0300

    sportStore project, example of online store

---
+++ src/components/Store.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col bg-dark text-white">
+        <a href="" class="navbar-brand">SPORT STORE</a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-3 bg-info p-2">
+        <h4 class="text-white m-2">Categories</h4>
+      </div>
+      <div class="col-9 p-2">
+        <product-list />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductList from '@/components/ProductList'
+export default {
+  components: {
+    ProductList
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 import store from './store'
+
+import './css/bootstrap.min.css'
+import './css/font-awesome-4.7.0/css/font-awesome.min.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   store,
   render: h => h(App)
 }).$mount('#app')
